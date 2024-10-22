e7df7216166160c6a94a0f6459b75c0b2a63f53f
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-10-22 15:45:58 -0600

    added search for single posters by id

--- spec/requests/api/v1/posters_request_spec.rb
+++ spec/requests/api/v1/posters_request_spec.rb
@@ -56,11 +56,51 @@ describe "api" do
 
       expect(poster).to have_key(:vintage) 
       expect(poster[:vintage]).to be_in([true, false]) 
-      
+
       expect(poster).to have_key(:img_url) 
       expect(poster[:img_url]).to be_a(String) 
     end
   
   end
+
+  it "shows posters by id" do
+
+    id = Poster.create(
+      name: "REGRET",
+      description: "Hard work rarely pays off.",
+      price: 89.00,
+      year: 2018,
+      vintage: true,
+      img_url: "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d"
+    ).id
+
+    get "/api/v1/posters/#{id}"
+
+    poster = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    expect(poster).to have_key(:id) 
+    expect(poster[:id]).to be_an(Integer)
+
+    expect(poster).to have_key(:name) 
+    expect(poster[:name]).to be_a(String)
+
+    expect(poster).to have_key(:description) 
+    expect(poster[:description]).to be_a(String) 
+
+    expect(poster).to have_key(:price) 
+    expect(poster[:price]).to be_a(Float)
+
+    expect(poster).to have_key(:year) 
+    expect(poster[:year]).to be_a(Integer) 
+
+    expect(poster).to have_key(:vintage) 
+    expect(poster[:vintage]).to be_in([true, false]) 
+
+    expect(poster).to have_key(:img_url) 
+    expect(poster[:img_url]).to be_a(String) 
+  end
+
 
 end
