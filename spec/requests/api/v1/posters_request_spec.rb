db40ef68770b23287927298b81e5472367af2166
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-10-22 14:00:20 -0600

    added tests to show all posters

---
+++ spec/requests/api/v1/posters_request_spec.rb
@@ -0,0 +1,66 @@
+require 'rails_helper'
+
+describe "api" do
+
+  it "shows all posters" do
+    Poster.create(
+      name: "REGRET",
+      description: "Hard work rarely pays off.",
+     price: 89.00,
+     year: 2018,
+     vintage: true,
+     img_url: "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d"
+    )
+
+    Poster.create(
+      name: "FAILURE",
+      description: "The key to success is knowing when to give up.",
+      price: 72.50,
+      year: 2020,
+      vintage: false,
+      img_url: "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d"
+    )
+
+    Poster.create(
+      name: "DESPAIR",
+     description: "Why try, when you can fail spectacularly?",
+     price: 65.99,
+      year: 2015,
+      vintage: true,
+      img_url: "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d"
+    )
+    
+    get "/api/v1/posters"
+
+    posters = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+  
+    expect(posters.count).to eq(3)
+
+    posters.each do |poster| 
+      expect(poster).to have_key(:id) 
+      expect(poster[:id]).to be_an(Integer)
+
+      expect(poster).to have_key(:name) 
+      expect(poster[:name]).to be_a(String)
+
+      expect(poster).to have_key(:description) 
+      expect(poster[:description]).to be_a(String) 
+
+      expect(poster).to have_key(:price) 
+      expect(poster[:price]).to be_a(Float)
+
+      expect(poster).to have_key(:year) 
+      expect(poster[:year]).to be_a(Integer) 
+
+      expect(poster).to have_key(:vintage) 
+      expect(poster[:vintage]).to be_in([true, false]) 
+      
+      expect(poster).to have_key(:img_url) 
+      expect(poster[:img_url]).to be_a(String) 
+    end
+  
+  end
+
+end
