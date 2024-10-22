e29342d864f6aaa3ed5fb6285604d2d1c0e432b7
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-10-22 15:45:31 -0600

    routed to controller for searching id

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   get "/api/v1/posters", to: "api/v1/posters#index"
-  # Defines the root path route ("/")
+  get "/api/v1/posters/:id", to: "api/v1/posters#show"
   # root "posts#index"
 end
