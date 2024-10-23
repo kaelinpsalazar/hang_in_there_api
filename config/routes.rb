588ea12ead06a258aa9d7a3d63b1dfed8fc3fb34
Kaelin Salazar <kaelinpsalazar@gmail.com>
2024-10-23 13:53:13 -0600

    update poster functionality implimented

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   get "/api/v1/posters", to: "api/v1/posters#index"
   post "/api/v1/posters", to: "api/v1/posters#create"
+  patch "/api/v1/posters/:id", to: "api/v1/posters#update"
   # Defines the root path route ("/")
   # root "posts#index"
 end
