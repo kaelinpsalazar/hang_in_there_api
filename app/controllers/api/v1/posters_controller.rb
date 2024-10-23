1d05cade1931ccf302a0ba4285ad54631c8f1d3d
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-10-23 14:30:55 -0600

    refactored for Serializer

--- app/controllers/api/v1/posters_controller.rb
+++ app/controllers/api/v1/posters_controller.rb
@@ -1,30 +1,30 @@
 class Api::V1::PostersController < ApplicationController
 
   def index
-    render json: Poster.all
+    render json: PosterSerializer.new(Poster.all)
   end
 
-
   def show
-    poster = Poster.find(params[:id])
-    render json: poster
+    render json: PosterSerializer.new(Poster.find(params[:id]))
   end
   
   def create
-    render json: Poster.create(poster_params)
+    render json: PosterSerializer.new(Poster.create(poster_params))
   end
+
   def update 
-    render json:Poster.update(params[:id], poster_params)
+    render json: PosterSerializer.new(Poster.update(params[:id], poster_params))
   end
-  private
-  
-  def poster_params
-    params.require(:poster).permit(:name, :description, :price, :year, :vintage, :img_url)
-
-  end
-
+ 
   def destroy
     poster = Poster.find(params[:id])
     poster.destroy
+  end
+  
+  private
+
+  def poster_params
+    params.require(:poster).permit(:name, :description, :price, :year, :vintage, :img_url)
+
   end
 end
