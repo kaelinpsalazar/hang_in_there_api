a54abf2e908d385c3be9a43ba54a8472c1547027
Kaelin Salazar <kaelinpsalazar@gmail.com>
2024-10-24 18:00:42 -0600

    validations are implimented

---
+++ spec/models/poster_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+describe Poster, type: :model do
+  describe "validations" do
+    it {should validate_presence_of(:name)}
+    it {should validate_uniqueness_of(:name)}
+    it {should validate_presence_of(:description)}
+    it {should validate_presence_of(:year)}
+    it {should validate_numericality_of(:year)}
+    it {should validate_presence_of(:price)}
+    it {should validate_numericality_of(:price)}
+    it {should validate_presence_of(:vintage)}
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -37,7 +37,12 @@ RSpec.configure do |config|
   config.fixture_paths = [
     Rails.root.join('spec/fixtures')
   ]
-
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
